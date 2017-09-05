1c3fb87a59ffe700315dc84a9df7a2fdb9b8702b
justJosh1004 <j.joson@live.com>
2017-09-05 12:35:46 -0400

    added a AlbumList and rendered it in app

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
-      //<View style={styles.container}>
+      <View>
         <Header headerText={'Albums'} />
-      //</View>
+        <AlbumList />
+      </View>
     );
   }
 }
-
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
