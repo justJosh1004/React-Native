f36fcdd6cac6be8e18a47e669e318e71cfd9dd01
justJosh1004 <j.joson@live.com>
2017-09-06 16:38:26 -0400

    added a click me button to the app

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image, image } = album;
@@ -16,22 +17,25 @@ const AlbumDetail = ({ album }) => {
     return (
         <Card>
             <CardSection>
-            <View style={thumbnailContainerStyle}>
-                <Image 
-                    style={thumbnailStyle} 
-                    source={{ uri: thumbnail_image }} 
-                />
-            </View>
-            <View style={headerContentStyle}>
-                <Text style={headerTextStyle}>{title}</Text>
-                <Text>{artist}</Text>
-            </View>
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle} 
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
             <CardSection>
                 <Image
                 style={imageStyle}
                     source={{ uri: image }} 
                 />
+            </CardSection>
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
