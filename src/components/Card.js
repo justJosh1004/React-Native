36598ade8ef0b9526721ecc60b1bc5f8a9203aba
justJosh1004 <j.joson@live.com>
2017-09-06 11:38:27 -0400

    added some styling to Cards.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,25 @@ import { View } from 'react-native';
 
 const Card = () => {
     return (
-        <View></View>
+        <View style={style.containerStyle}></View>
     );
+};
+
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { Width: 0, Height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
 };
 
 export default Card;
